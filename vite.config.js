1d1477f12a5c0e37d583fda48576824d787c6902
bcoop247 <brcooper247@gmail.com>
2023-10-30 19:35:41 -0400

    update base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Wise-Eye-Weather/',
   plugins: [react()],
-  base: "/http://bcoop247.github.io/Wise-Eye-Weather/",
 })
