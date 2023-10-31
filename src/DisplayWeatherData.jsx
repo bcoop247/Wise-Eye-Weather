e564e3a4dadb4420f5187c646e1e80cdf0f58ccb
bcoop247 <brcooper247@gmail.com>
2023-10-30 20:26:46 -0400

    flex-wrap

--- src/DisplayWeatherData.jsx
+++ src/DisplayWeatherData.jsx
@@ -115,12 +115,10 @@ return (
  
 {weatherData && weatherData.daily && Array.isArray(weatherData.daily) ? (
   weatherData.daily.map((dayData, index) => (
-    <div key={index} id='eightDayForecast' className='container text-center rounded'>
+    <div key={index} id='eightDayForecast' className='container flex-wrap text-center rounded'>
       <p className='text-center'>{eightDates[index]}</p>
       <img src={`https://openweathermap.org/img/wn/${dayData.weather[0].icon}@2x.png`} alt='Weather Icon'  />
       <p className='text-center'>{Math.floor((dayData.temp.max - 273.15) * 9/5 + 32)}&deg; / {Math.floor((dayData.temp.min - 273.15) * 9/5 + 32)}&deg;</p>
-      {/* <p>Humidity: {dayData.humidity}</p> */}
-      {/* Add more properties as needed */}
       <div className='d-flex justify-content-center'> 
       <svg onClick={() => toggleMoreWeatherData(index)} id="showMoreWeatherDataIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-square" viewBox="0 0 16 16">
       <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
@@ -134,6 +132,8 @@ return (
 )}
 
 </div>
+
+
 {index != null && showMoreWeatherData && (<div id="showMoreWeatherDataDiv" className='container border border-black border-3 justify-content-center align-items-center rounded'> 
 
 <p className='text-center' id="underline">{longDates[index]}</p>
