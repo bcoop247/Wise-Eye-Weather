77aff540c9d4af54c98ac8f101724bafd62fa319
bcoop247 <brcooper247@gmail.com>
2023-10-31 10:52:13 -0400

    Added Title

--- src/DisplayWeatherData.jsx
+++ src/DisplayWeatherData.jsx
@@ -111,27 +111,6 @@ return (
   </h4>
   </div>
  
-  {/* <div className='container border-primary d-flex justify-content-center align-items-center'>
- 
-{weatherData && weatherData.daily && Array.isArray(weatherData.daily) ? (
-  weatherData.daily.map((dayData, index) => (
-    <div key={index} id='eightDayForecast' className='container flex-wrap text-center rounded'>
-      <p className='text-center'>{eightDates[index]}</p>
-      <img src={`https://openweathermap.org/img/wn/${dayData.weather[0].icon}@2x.png`} alt='Weather Icon'  />
-      <p className='text-center'>{Math.floor((dayData.temp.max - 273.15) * 9/5 + 32)}&deg; / {Math.floor((dayData.temp.min - 273.15) * 9/5 + 32)}&deg;</p>
-      <div className='d-flex justify-content-center'> 
-      <svg onClick={() => toggleMoreWeatherData(index)} id="showMoreWeatherDataIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-square" viewBox="0 0 16 16">
-      <path d="M3.626 6.832A.5.5 0 0 1 4 6h8a.5.5 0 0 1 .374.832l-4 4.5a.5.5 0 0 1-.748 0l-4-4.5z"/>
-      <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2z"/>
-      </svg>
-      </div>
-    </div>    
-  ))
-) : (
-  <p>Weather data is not available.</p>
-)}
-
-</div> */}
 <div className='container'>
   {weatherData && weatherData.daily && Array.isArray(weatherData.daily) ? (
     <div className='row'>
